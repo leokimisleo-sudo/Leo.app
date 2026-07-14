c5d3d46e4c0702362394a307495bbcbe774fafee
Leo <leokimisleo@gmail.com>
2026-07-14 19:52:23 +0000

    my first project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("🎈 My name is Leo Kim")
+st.write( "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
